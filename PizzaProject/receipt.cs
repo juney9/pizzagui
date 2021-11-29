55bdfa98dae22f76662dcd1240726110064643ce
juney9 <94068751+juney9@users.noreply.github.com>
2021-11-28 19:03:18 -0500

    s

--- PizzaProject/receipt.cs
+++ PizzaProject/receipt.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -19,25 +19,4 @@ namespace PizzaProject
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace PizzaProject
-{
-    public partial class receipt : Form
-    {
-        public receipt()
-        {
-            InitializeComponent();
-        }
-    }
-}
->>>>>>> bd0494b1d1e771208036917194d68bf43350aa7f
